61e5db72b8dc0981760e8a9eb85dc0ec92af3559
정민재 <jmj@jeongminjaeui-MacBookPro.local>
2019-10-20 04:02:01 +0900

    RecipeResearchingListAdapter 주석 추가

--- RecipeLab/app/src/main/java/com/example/recipelab/adapter/RecipeResearchingListAdapter.kt
+++ RecipeLab/app/src/main/java/com/example/recipelab/adapter/RecipeResearchingListAdapter.kt
@@ -11,12 +11,16 @@ import kotlinx.android.synthetic.main.item_recipe_elements.view.*
 
 class RecipeResearchingListAdapter(var data: ArrayList<Research>) :
     RecyclerView.Adapter<RecipeResearchingListAdapter.ViewHolder>() {
+    // 레시피의 상세 페이지로 들어갔을때 연구 목록을 보여주는 어댑터
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflate(R.layout.~~) 부분에서 레이아웃 외에는 건드릴 부분이 없음,
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe_elements,parent,false)
 
         return ViewHolder(view)
     }
 
+    // 기본값. 건드릴부분이 없음.
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
         val listener = View.OnClickListener {
@@ -35,6 +39,7 @@ class RecipeResearchingListAdapter(var data: ArrayList<Research>) :
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
         val view = v
         fun bind(listener: View.OnClickListener, item: Research) {
+            // 이 부분에서 컴포넌트와 데이터를 연결해주면 됨.
             view.text_elements_1.text = item.date
             view.text_elements_2.text = item.bean
             view.text_elements_3.text = item.water
