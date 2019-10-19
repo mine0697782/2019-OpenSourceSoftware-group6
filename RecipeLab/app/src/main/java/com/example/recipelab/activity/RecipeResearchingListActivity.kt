45fe11b7bf10e35f36940963a2d3fef38a6daacb
정민재 <jmj@jeongminjaeui-MacBookPro.local>
2019-10-20 03:15:01 +0900

    불필요한 코드 삭제, RecipeResearchingActivity 파일에 주석 추가

--- RecipeLab/app/src/main/java/com/example/recipelab/activity/RecipeResearchingListActivity.kt
+++ RecipeLab/app/src/main/java/com/example/recipelab/activity/RecipeResearchingListActivity.kt
@@ -37,6 +37,7 @@ class RecipeResearchingListActivity : AppCompatActivity(),
 
     var data: ArrayList<Research> = arrayListOf()
 
+    // 메인 액티비티에서 클릭된 아이템의 고유 번호로, 데이터베이스에서 정보를 찾기 위한 키 (Primary Key)
     var key: Long? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,17 +46,20 @@ class RecipeResearchingListActivity : AppCompatActivity(),
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         val fab = fab_add_research
+        // 오른쪽 하단의 버튼 클릭 이벤트 리스너. 누르면 연구 기록 추가 페이지로 넘어가며, 현재 보고있는 레시피의 고유번호를 전달
         fab.setOnClickListener {
             startActivity(Intent(this,AddResearchActivity::class.java).putExtra("key",key))
         }
 
+        // 어탭터 선언부. 연구 기록을 보여주는 어댑터
         adapter = RecipeResearchingListAdapter(data)
         recyclerView = rv_list_researching_elements
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+        // 어댑터에 가로로 구분선 추가
         recyclerView.addItemDecoration(
             DividerItemDecoration(this,
-                DividerItemDecoration.HORIZONTAL)
+                DividerItemDecoration.VERTICAL)
         )
 
         Realm.init(this)
@@ -70,14 +74,18 @@ class RecipeResearchingListActivity : AppCompatActivity(),
         textTag = text_research_coffee_tag1
 
         realm.beginTransaction()
-
+        // 메인 액티비티에서 인텐트로 전달받은 정보를 받는 변수
         val bundle = intent.extras
+        // 미리 선언해둔 변수에 전달받은 데이터를 찾아서 넣어준다
         key = bundle?.getLong("key")
+        // 전달받은 고유번호(레시피 번호)로 레시피의 정보를 찾아온다 (제목, 날짜 등)
         val item = realm.where(ResearchTamplet::class.java).equalTo("id",key).findFirst()
 
+        // 상단의 텍스트를 설정
         textName.text = item?.menu
         textDate.text = item?.date
 
+        // 레시피 번호가 포함된 연구 기록을 찾아서 어댑터에 넣어준다
         adapter.data.addAll(realm.where(Research::class.java).equalTo("recipeNum",key).findAll())
         adapter.notifyDataSetChanged()
 
@@ -86,7 +94,6 @@ class RecipeResearchingListActivity : AppCompatActivity(),
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.researching, menu)
         return true
     }
@@ -94,6 +101,7 @@ class RecipeResearchingListActivity : AppCompatActivity(),
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.action_finish -> {
+                // 데이터베이스에서 현재 보고있는 레시피를 찾아 연구 완료 여부를 참으로 바꿈
                 realm.beginTransaction()
                 val item = realm.where(ResearchTamplet::class.java).equalTo("id",key).findFirst()
                 item?.finished = true
@@ -103,6 +111,7 @@ class RecipeResearchingListActivity : AppCompatActivity(),
             }
 
             R.id.action_delete -> {
+                // 데이터베이스에서 현재 보고있는 레시피를 찾아 삭제함
                 realm.beginTransaction()
                 val item = realm.where(ResearchTamplet::class.java).equalTo("id",key).findFirst()
                 item?.deleteFromRealm()
@@ -112,12 +121,13 @@ class RecipeResearchingListActivity : AppCompatActivity(),
             }
 
             R.id.action_refresh -> {
+                // 어댑터의 데이터를 비우고 데이터베이스에서 다시 새로 꺼내 넣어줌 (갱신)
                 realm.beginTransaction()
                 adapter.data.clear()
                 adapter.data.addAll(realm.where(Research::class.java).equalTo("recipeNum",key).findAll())
                 adapter.notifyDataSetChanged()
                 realm.commitTransaction()
-                Toast.makeText(this,"sibal",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"refresh",Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -125,8 +135,6 @@ class RecipeResearchingListActivity : AppCompatActivity(),
     }
 
     override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
         return true
     }
 
